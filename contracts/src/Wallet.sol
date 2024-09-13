0a47c9d31db36c629ade31245bd8639750347daa
yashtotla <yashtotla98@gmail.com>
2024-09-14 02:49:42 +0530

    :sparkles: wallet constructor

---
+++ contracts/src/Wallet.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
+
+contract Wallet {
+    address public immutable walletFactory;
+    IEntryPoint private immutable _entryPoint;
+
+    constructor(IEntryPoint anEntryPoint, address ourWalletFactory) {
+        _entryPoint = anEntryPoint;
+        walletFactory = ourWalletFactory;
+    }
+}
