bec82eba7fd895270bba16b254cb6406f006aab6
yashtotla <yashtotla98@gmail.com>
2024-09-14 12:26:21 +0530

    :memo: remappings, wallet entry point and validate sig

--- contracts/src/Wallet.sol
+++ contracts/src/Wallet.sol
@@ -3,12 +3,39 @@ pragma solidity ^0.8.12;
 
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
 
-contract Wallet {
+import {BaseAccount} from "account-abstraction/core/BaseAccount.sol";
+import {UserOperation} from "account-abstraction/interfaces/UserOperation.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract Wallet is BaseAccount {
+    
+    using ECDSA for bytes32;
+
     address public immutable walletFactory;
     IEntryPoint private immutable _entryPoint;
+    address[] public owners;
 
     constructor(IEntryPoint anEntryPoint, address ourWalletFactory) {
         _entryPoint = anEntryPoint;
         walletFactory = ourWalletFactory;
+    }
+
+    function entryPoint() public view override returns (IEntryPoint) {
+        return _entryPoint;
+    }
+
+    function _validateSignature(
+        UserOperation calldata userOp,
+        bytes32 userOpHash
+    ) internal view override returns (uint256) {
+        bytes32 hash = userOpHash.toEthSignedMessageHash();
+        bytes[] memory signatures = abi.decode(userOp.signature, (bytes[]));
+
+        for (uint256 i = 0; i < owners.length; i++) {
+            if (owners[i] != hash.recover(signatures[i])) {
+                return SIG_VALIDATION_FAILED;
+            }
+        }
+        return 0;
     }
 }
